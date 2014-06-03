8327d54923ff41fa4580a5c59d70f42c9338af28
bj <bjpaat01@gmail.com>
2014-06-03 14:36:57 +0800

    added how to page

--- blox.py
+++ blox.py
@@ -12,27 +12,41 @@ app = Flask(__name__)
 # pulls in app configuration by looking for UPPERCASE variables
 app.config.from_object(__name__)
 
+def ip_addresses():
+    f = open('config', 'r')
+    ALL_IP = eval(f.read())
+    f.close()
+    IP = False
+    if ALL_IP['wlan'] != '0.0.0.0':
+        IP = ALL_IP['wlan']
+    else:
+        IP = ALL_IP['eth']
+    ALL_IP['IP'] = IP
+    return ALL_IP
 
 @app.route('/')
 def index():
-	f = open('config', 'r')
-	ALL_IP = eval(f.read())
-	f.close()
-	IP = False
-	if ALL_IP['wlan'] != '0.0.0.0':
-	    IP = ALL_IP['wlan']
-	else:
-	    IP = ALL_IP['eth']
-	dicts = {
-	    'ALL_IP': ALL_IP,
-	    'IP': IP,
-	    'db': DATABASE,
-	    }
-	return render_template('index.html', dicts=dicts)
+    ALL_IP = ip_addresses()
+    dicts = {
+        'ALL_IP': ALL_IP,
+        'IP': ALL_IP['IP'],
+        }
+    return render_template('index.html', dicts=dicts)
 
 @app.route('/contactus')
 def contactus():
-	return render_template('contactus.html')
+    ALL_IP = ip_addresses()
+    return render_template('contactus.html',dicts=ALL_IP)
+
+@app.route('/howto')
+def howto():
+    ALL_IP = ip_addresses()
+    dicts = {
+        'All_IP': ALL_IP,
+        'IP': ALL_IP['IP'] ,
+        }
+    return render_template('howto.html', dicts=dicts)
 
 if __name__ == '__main__':
-	app.run(debug=False)
+    app.debug = True
+    app.run(host='0.0.0.0')
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~/Projects/blox
+source venv/bin/activate
+gunicorn -b 0.0.0.0:5000 blox:app &
