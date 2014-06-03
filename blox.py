fcbb365227bd8341497ad75f88e5eda88ac1183d
bj <bjpaat01@gmail.com>
2014-06-03 20:55:23 +0800

    added details on how to

--- blox.py
+++ blox.py
@@ -4,7 +4,6 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 	render_template
 
 # configuration
-
 DATABASE = False
 
 # create our little application
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~/Projects/blox
 source venv/bin/activate
-gunicorn -b 0.0.0.0:5000 blox:app &
+gunicorn -b 0.0.0.0:5000 blox:app -w 4 &
