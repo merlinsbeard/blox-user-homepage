0ee171cd772813855cb82ec1bbca867574b6df95
bj <bjpaat01@gmail.com>
2014-06-10 21:51:16 +0800

    blox py  added function to check if using desktops or mobile index.html  removed the desktop sections  change links of services to how tos exept for plex music.html  new music page. contains links to apps and also links to how to user-agent.html  used to check on user agent information

--- blox.py
+++ blox.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-	render_template
+	render_template, request
 
 # configuration
 DATABASE = False
@@ -23,14 +23,66 @@ def ip_addresses():
     ALL_IP['IP'] = IP
     return ALL_IP
 
+def using_desktop():
+    desktop_os = ['bsd','linux','macos','solaris','windows']
+    smartphones = ['android','iphone','ipad']
+    userAgentString = request.headers.get('User-Agent')
+    user_agent = request.user_agent
+    platform = user_agent.platform
+    desktop = False
+    if platform in desktop_os:
+        desktop = True
+    return desktop
+
+
 @app.route('/')
 def index():
     ALL_IP = ip_addresses()
     dicts = {
         'ALL_IP': ALL_IP,
         'IP': ALL_IP['IP'],
+        'using_desktop': using_desktop(),
         }
     return render_template('index.html', dicts=dicts)
+
+desktop = ['bsd','linux','macos','solaris','windows']
+smartphones = ['android','iphone','ipad']
+
+
+
+@app.route('/user-agent')
+def getUserAgent():
+    userAgentString = request.headers.get('User-Agent')
+    user_agent = request.user_agent
+    platform = user_agent.platform
+    linux = False
+    windows = False
+    android = False
+    desktop_or_laptop = False
+    smartphone_or_others = False
+    ua = dir(user_agent)
+
+    desktops = False
+
+    if platform in desktop:
+        desktop_or_laptop = True
+        desktops = True
+
+    if platform in smartphones:
+        smartphone_or_others = True
+
+    return render_template('user-agent.html', userAgentString=userAgentString,
+        user_agent=user_agent, platform=platform, linux=linux, ua=ua, desktops=desktops)
+
+@app.route('/music')
+def music():
+    ALL_IP = ip_addresses()
+    dicts = {
+        'ALL_IP': ALL_IP,
+        'IP': ALL_IP['IP'],
+        }
+
+    return render_template('music.html', dicts=dicts)
 
 @app.route('/contactus')
 def contactus():
--- blox.wsgi
+++ blox.wsgi
@@ -1,4 +1,4 @@
-activate_this = '/path/to/env/bin/activate_this.py'
+activate_this = 'home/blackbox/Projects/blox/venv/bin/activate'
 execfile(activate_this, dict(__file__=activate_this))
 from blox import app as application
 
