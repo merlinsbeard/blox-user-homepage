074b950dc81cc14969625d7bcf392400910d7187
bj paat <bjpaat01@gmail.com>
2014-10-03 15:04:18 +0800

    added Download section

--- blox.py
+++ blox.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-	render_template, request, flash
+	render_template, request, flash, send_from_directory
 from werkzeug.utils import secure_filename
 from mpd import MPDClient
 import subprocess
@@ -498,6 +498,25 @@ def multiple():
     '''
 
     return render_template('multiple.html')
+
+_file_directory = '/home/blox/Downloads/'
+@app.route('/download')
+def download_directory():
+    """
+        Shows files in the Download directory of blox
+    """
+    output_html = '<html>{0}</html>'
+    href_tag = '<a href="/download/{0}">{0}</a><br>'
+    file_links = ''
+    for filename in os.listdir(_file_directory):
+        if os.path.isfile(_file_directory + filename):
+            file_links += href_tag.format(filename)
+    return output_html.format(file_links)
+
+@app.route('/download/<path:filename>')
+def send_file(filename):
+    # Makes the files downloadable directyle
+    return send_from_directory(_file_directory, filename)
 
 
 if __name__ == '__main__':
